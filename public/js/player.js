6a5b5f3e58de1f5ad801799825cadb5eeac2ead6
karensantos <karensantosrocha@gmail.com>
2014-03-25 13:52:39 -0300

    implementando player

--- public/js/player.js
+++ public/js/player.js
@@ -1,63 +1,77 @@
-/**
- * 
- */
-function closePlaylist() {
-	document.getElementById("faixaPlayer").style.display = "none";
-//	$('#faixaPlayer').style.display = "none";
-}
+var tag = document.createElement('script');
+tag.src = "//www.youtube.com/iframe_api";
+var firstScriptTag = document.getElementsByTagName('script')[0];
+firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
 
 function openPlaylist(id) {
-	
-	$.ajax({
-		  type: "GET",
-		  url: "/getPlaylist/" + id,
-		  data: "",
-		  success: function(data, textStatus, jqXHR){
-			  document.getElementById("faixaPlayer").style.display = "block";
-			  document.getElementById("video").innerHTML = "<iframe width='300' height='59' " +
-						"src='//www.youtube.com/embed/" + data.musicas[0].link + "?rel=0&amp;fs=0&amp;autoplay=1' allowfullscreen='false' frameborder='0'></iframe>";
-			  document.getElementById("playlistPlayer").innerHTML = data.nome;
-			  document.getElementById("musicaAtual").innerHTML = '"' + data.musicas[0].nome + '"' + "<br>" + data.musicas[0].artista;
-			  //			  console.log(data.nome);
-		  },
-		  error: function(XMLHttpRequest, textStatus, errorThrown) {
 
-		  }
+	$.ajax({
+		type : "GET",
+		url : "/getPlaylist/" + id,
+		data : "",
+		success : function(playlist, textStatus, jqXHR) {
+			playlistStart(playlist);
+		},
+
+		error : function(XMLHttpRequest, textStatus, errorThrown) {
+		}
 	});
-	
+
 }
 
-//// Script para tocar o video sem ele aparecer. iFrame tem tamanho zero
-//					
-//var loadLinkH = document.getElementById("playlist"); // armazena o id do link
-//loadLinkH.onclick = loadFrame; // chama a função loadFrame quando clica no link
-//					
-//function loadFrame(e) { // função para criar o iFrame
-//	var containerHiden = document.getElementById("frameHiden"); // armazena o id da div onde vai aparecer o iFrame
-//	var containerFaixa = document.getElementById("player");
-//	e = e || window.event; 
-//						
-//	//adiciona o iFrame na div
-//	containerHiden.innerHTML = "<iframe width='0' height='0' src=\"" + loadLinkH.href + "\" frameborder='0' allowfullscreen >";
-//	document.getElementById("faixaPlayer").style.display = "block";
-//	//containerFaixa.innerHTML = "<div id='faixaPlayer'></div>"; 
-//	
-//	//chama a função stopEvent
-//	stopEvent( e ); 
-//}
-//
-////A funcao stopEvent abaixo inpede a ação primaria do tag <a>.
-////Assim o link funciona mesmo se o Javascript estiver desativado.
-//
-//function stopEvent( e ) {
-//	e = e || window.event; 
-//	
-//	e.cancelBubble = true; 
-//	e.returnValue = false; 
-//
-//	if( e.stopPropagation ) { 
-//	 e.stopPropagation(); 
-//	 e.preventDefault(); 
-//	} 
-//	return false; 
-//} 
+function playlistStart(playlist) {
+	
+	alert(playlist.musicas.length);
+	alert(playlist.musicas[0].link);
+
+	document.getElementById("faixaPlayer").style.display = "block";
+
+	var videoIDs = [];
+	var player, currentVideoId = 0;
+
+	for (var i = 0; i < playlist.musicas.length; i++){
+		videoIDs.push(playlist.musicas[i].link);
+	}
+	
+	function onYouTubeIframeAPIReady() {
+		player = new YT.Player('player', {
+			height : '59',
+			width : '300',
+			events : {
+				'onReady' : onPlayerReady,
+				'onStateChange' : onPlayerStateChange
+			}
+		});
+	}
+
+	function onPlayerReady(event) {
+		event.target.loadVideoById(videoIDs[currentVideoId]);
+	}
+
+	function onPlayerStateChange(event) {
+		if (event.data == YT.PlayerState.ENDED) {
+			currentVideoId++;
+			if (currentVideoId < videoIDs.length) {
+				player.loadVideoById(videoIDs[currentVideoId]);
+			}
+		}
+	}
+	
+	onYouTubeIframeAPIReady();
+
+	// document.getElementById("video").innerHTML = "<iframe width='300'
+	// height='59' " +
+	// "src='//www.youtube.com/embed/" + data.musicas[0].link +
+	// "?rel=0&amp;fs=0&amp;autoplay=1' allowfullscreen='false'
+	// frameborder='0'></iframe>";
+
+	document.getElementById("playlistPlayer").innerHTML = playlist.nome;
+	document.getElementById("musicaAtual").innerHTML = '"'
+			+ playlist.musicas[0].nome + '"' + "<br>"
+			+ playlist.musicas[0].artista;
+}
+
+function closePlaylist() {
+	document.getElementById("faixaPlayer").style.display = "none";
+	// $('#faixaPlayer').style.display = "none";
+}
