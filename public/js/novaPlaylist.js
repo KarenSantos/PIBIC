d035e3be885e3cac20587fbd9aa3a1c4872cc46c
KarenSantos <karensantosrocha@gmail.com>
2014-04-28 21:35:22 -0300

    added songs showing on div

--- public/js/novaPlaylist.js
+++ public/js/novaPlaylist.js
@@ -2,9 +2,12 @@
  * 
  */
 
-var paisagem1;
-var paisagem2;
-var transicao;
+var paisagem1 = {};
+var paisagem2 = {};
+var transicao = [];
+var numMusP1 = 0;
+var numMusP2 = 0;
+var numMusT = 0;
 
 function skip(){
 	document.getElementById("bskip").style.display = "none";
@@ -55,7 +58,6 @@ function adicionarMusica() {
 		} else {
 			
 			var APIKey = "AIzaSyDrBArU_WZxTHmQc62_YdktmhQsFZ4PnAg";
-			
 			var videoId = link.split("=")[1].split("&")[0];
 			
 			$.ajax({
@@ -63,9 +65,37 @@ function adicionarMusica() {
 				url : "https://www.googleapis.com/youtube/v3/videos?part=snippet&id=" + videoId + "&key=" + APIKey,
 				datatype : "application/json",
 				success : function(data) {
-					alert("chegou no sucess");
 					
-					alert(data.items[0].title);
+					var videoTitle = data.items[0]['snippet']['title'];
+					
+					if (musicGoTo == "1"){
+						numMusP1++;
+						var musica = "musica" + numMusP1;
+						paisagem1[musica] = [videoTitle, videoId];
+					
+					} else if (musicGoTo == "2") {
+						
+						if(numMusT > 0){
+							var trocar = confirm("Você já adicionou uma música de transição, deseja substituí-la?");
+							if (trocar){
+								transicao = [videoTitle, videoId];
+							}
+							
+						} else {
+							numMusT++;
+							transicao = [videoTitle, videoId];
+						}
+						
+					
+					} else {
+						numMusP2++;
+						var musica = "musica" + numMusP2;
+						paisagem2[musica] = [videoTitle, videoId];
+					}
+					
+					document.getElementById("link").value = ""; //limpando o text input do link
+					exibirMusicas();
+									
 				},
 				error : function(XMLHttpRequest, textStatus, errorThrown) {
 					alert("Ocorreu um erro. Tente mais tarde.");
@@ -99,6 +129,42 @@ function drop(ev, goTo){
 
 	alert("link: " + data + "\n enviado para: " + goTo.id);
 	
+}
+
+function exibirMusicas() {
+	
+	document.getElementById("p1music").innerHTML = "";
+	for (var p1mus in paisagem1){
+		document.getElementById("p1music").innerHTML += "<button type='button'>" + paisagem1[p1mus][0] + "</button>";
+	}
+	
+	if (transicao.length > 0){
+		document.getElementById("tmusic").innerHTML = "<button type='button'>" + transicao[0] + "</button>";
+	}
+	
+	document.getElementById("p2music").innerHTML = "";
+	for (var p2mus in paisagem2){
+		document.getElementById("p2music").innerHTML += "<button type='button'>" + paisagem2[p2mus][0] + "</button>";
+	}
+		
+}
+
+function proximo(){
+	
+	var genero1 = document.getElementById("genero1").value;
+	var genero2 = document.getElementById("genero2").value;
+	
+	var nome = document.getElementById("playlistName").value;
+	
+	if (numMusP1 >= 3 && numMusP2 >=3){
+		if (numMusT == 1){
+			alert("ok vc pode ir");
+		} else {
+			alert("Insira uma música de transição entre as paisagens.")
+		}
+	} else {
+		alert("Suas paisagens devem ter pelo menos 3 músicas.")
+	}
 }
 
 $("#keyword").keyup(function(event){
